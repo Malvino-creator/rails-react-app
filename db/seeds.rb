2223cc8a4f913cb4a2846e01d3991c6daca72296
Moringa School <malvin.opondo@student.moringaschool.com>
2022-12-08 02:14:59 +0300

    feat: create our first model and database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = ['James Kent', 'Phineas Carl', 'Pascal Mwai', 'Darrel Jones']
+users.each{|user| User.create(name: user, age: 30 description: "I am a happy client #{user}.")}
