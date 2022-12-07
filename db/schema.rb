d450cd73779136e52e7dc54f41490e173047181d
Moringa School <malvin.opondo@student.moringaschool.com>
2022-12-08 02:46:00 +0300

    feat:Populate our seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_225855) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_232418) do
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.text "specialization"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
